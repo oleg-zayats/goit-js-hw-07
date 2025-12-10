d86b6074d61613891ea4cb76e8b84aa0e73505b8
Олег <ozaac20@gmail.com>
2025-12-10 22:14:03 +0200

    g

--- js/task-2.js
+++ js/task-2.js
@@ -1,27 +1,27 @@
 const images = [
   {
-    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
-    alt: "White and Black Long Fur Cat",
+    url: "./images/1.jpg"  ,
+    alt: "People discussing ideas around a table with a laptop",
   },
   {
-    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
-    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+    url: "./images/2.jpg",
+    alt: "Hand holding a glowing lightbulb against a colored background",
   },
   {
-    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
-    alt: "Group of Horses Running",
+    url: "./images/3.jpg",
+    alt: "A person holding a smartphone next to a potted plant",
   },
   {
-    url: "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
-    alt: "Alpine Spring Meadows",
+    url: "./images/4.jpg",
+    alt: "Modern open-plan office space with desks and chairs",
   },
   {
-    url: "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
-    alt: "Nature Landscape",
+    url: "./images/5.jpg",
+    alt: "Bright collaborative workspace with unique furniture",
   },
   {
-    url: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
-    alt: "Lighthouse Coast Sea",
+    url: "./images/6.jpg",
+    alt: "Long table in a conference room with large windows",
   },
 ];
 
